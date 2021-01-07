d257a85932c10a51baba129414718fdab1f2054f
nxblnd <52958704+nxblnd@users.noreply.github.com>
2021-01-08 00:32:15 +0300

    Basic functionality achieved: playing sound after delay on certain pages

--- background.js
+++ background.js
@@ -1,11 +1,16 @@
 let listOfSites = ["https://twitter.com/"];
 let domains = listOfSites.map(url => new URL(url).hostname);
+let timeLimit = 1;
 
 browser.runtime.onConnect.addListener(connect);
 
 function connect(port) {
     let hostname = new URL(port.sender.url).hostname;
     if (domains.includes(hostname)) {
-        port.postMessage();
+        port.postMessage({timeLimit: timeLimit});
     }
+    port.onMessage.addListener(() => {
+        let audio = new Audio(browser.extension.getURL("soundtest.wav"));
+        audio.play();
+    })
 }
--- screamer.js
+++ screamer.js
@@ -1,8 +1,13 @@
 console.log("screamer");
 
-let background = browser.runtime.connect({name: "content script"});
+let background = browser.runtime.connect({name: 'content script'});
 background.onMessage.addListener(timer);
 
-function timer() {
+let currentTime = 0;
 
+function timer(msg) {
+    setTimeout(() => console.log('timeout'), msg.timeLimit);
+    background.postMessage();
+    // let audio = new Audio(browser.extension.getURL("note.mp3"));
+    // audio.play();
 }
