1b5a94e67c317701d896cb4a4715035e7b5a7f06
nxblnd <52958704+nxblnd@users.noreply.github.com>
2021-01-09 22:41:13 +0300

    Loading options in background.js

--- background.js
+++ background.js
@@ -1,26 +1,71 @@
-let listOfSites = ["https://twitter.com/"];
-let domains = listOfSites.map(url => new URL(url).hostname);
-let timeLimit = 2000;
-let audio = new Audio(browser.extension.getURL("soundtest.wav"));
-audio.loop = true;
+const msInMinute = 60 * 1000;
+const defaultOptions = {
+    timeLimit: 5 * msInMinute,
+    listOfSites: [],
+    sound: 'antivirusPig',
+    loop: true
+}
+const soundboard = {
+    antivirusPig: browser.extension.getURL("soundboard/antivirusPig.ogg"),
+    nuclearLaunch: browser.extension.getURL("soundboard/nuclearLaunch.ogg"),
+    alarmClock: browser.extension.getURL("soundboard/alarmClock.ogg")
+}
+
+let options;
+let domains;
+let sound;
+
+browser.storage.sync.get()
+    .then(applyOptions)
+    .then(convertListOfSites)
+    .then(loadAudio)
+    .catch(error => console.log(error));
 
 browser.runtime.onConnect.addListener(connect);
 
 function connect(port) {
     let hostname = new URL(port.sender.url).hostname;
     if (domains.includes(hostname)) {
-        port.postMessage({timeLimit: timeLimit});
+        port.postMessage({timeLimit: options.timeLimit});
     }
     port.onMessage.addListener(dispatchAudio);
 }
 
 function dispatchAudio(msg) {
     if (msg.audio === "play") {
-        audio
+        sound
             .play()
             .catch(error => console.log(error));
     } else if (msg.audio === "stop") {
-        audio.pause();
-        audio.currentTime = 0;
+        sound.pause();
+        sound.currentTime = 0;
     }
+}
+
+function applyOptions(loadedOptions) {
+
+    // if result is empty object
+    if (Object.keys(loadedOptions).length === 0 && loadedOptions.constructor === Object) {
+        options = defaultOptions;
+    } else {
+        options = loadedOptions;
+    }
+}
+
+function convertListOfSites() {
+
+    function processUrl(url) {
+        try {
+            return new URL(url).hostname;
+        } catch {
+            return url;
+        }
+    }
+
+    domains = options.listOfSites.map(processUrl)
+}
+
+function loadAudio() {
+    sound = new Audio(soundboard[options.sound]);
+    sound.loop = options.loop;
 }
